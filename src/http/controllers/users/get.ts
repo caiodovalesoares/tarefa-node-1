e3acdc9a576ed5362f9ebf2209919aee8ed2fcf2
CaiodoValeSoares <caiodovalesoares@gmail.com>
2025-02-28 16:35:58 -0300

    get user

---
+++ src/http/controllers/users/get.ts
@@ -0,0 +1,30 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found";
+import { UserAlreadyExists } from "@/use-cases/errors/user-already-exists-error";
+import { GetUserCase } from "@/use-cases/get-user-use-case";
+import { RegisterUseCase } from "@/use-cases/register-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function get(request: FastifyRequest, reply: FastifyReply) {
+    const getParamsSchema = z.object({
+        userId: z.string().uuid()
+    })
+
+    const { userId } = getParamsSchema.parse(request.params)
+
+    try {
+        const prismaUsersRepository = new PrismaUsersRepository()
+        const getUserUseCase = new GetUserCase(prismaUsersRepository)
+        const user = await getUserUseCase.execute({
+            userId
+        })
+
+        return reply.status(200).send(user)
+    } catch (err) {
+        if (err instanceof ResourceNotFoundError) {
+            return reply.status(404).send({ message: err.message})
+        }
+        throw err
+    }
+}
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,11 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
+import { get } from "./get";
 
 export function userRoutes(app: FastifyInstance) {
     app.post('/users', register)
     app.post('/authenticate', authenticate)
+
+    app.get('/users/:userId', get)
 }
