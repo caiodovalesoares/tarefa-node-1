71b34f869be71afb9924e909e51964e0d8aedba0
CaiodoValeSoares <caiodovalesoares@gmail.com>
2025-03-03 19:28:15 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import fastify from "fastify";
 import { userRoutes } from "./http/controllers/users/routes";
-import { ZodError } from "zod";
+import { any, ZodError } from "zod";
 import { postRoutes } from "./http/controllers/posts/routes";
 import fastifyJwt from "@fastify/jwt";
 import fastifyCookie from "@fastify/cookie";
 import { env } from "./env";
+import fastifyCors from "@fastify/cors";
 
 export const app = fastify()
+
+app.register(fastifyCors, {
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+})
 
 app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
